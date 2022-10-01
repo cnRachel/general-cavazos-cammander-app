4f660136bd640acdb8ddcadc7ba9c33c4810159a
Rachel Chen <rachelchen110@gmail.com>
2022-10-01 16:28:26 -0700

    files are able to run on my computer!

--- commander_app/src/main/java/com/example/App.java
+++ commander_app/src/main/java/com/example/App.java
@@ -1,13 +1,58 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Random;
+import org.json.simple.*;
+
+public class App {
+
+  public static void main(String[] args) {
+    String fileName =
+      "/Users/rache/Documents/GitHub/general-cavazos-cammander-app/commander_app/src/main/java/com/example/commands.json";
+
+    // read coammands
+    JSONArray commandJSONArray = JSONFile.readArray(fileName);
+    String[] commandArray = getCommandArray(commandJSONArray);
+    System.out.println(commandArray);
+
+    // print list of all commands
+    System.out.println("----- List of all commands -----");
+    print(commandArray);
+
+    System.out.println(
+      "----- Issuing 5 random commands from General Cavazos -----"
+    );
+    randomCommand(commandArray, 5);
+  }
+
+  // randomly issue commands from General Cavazos
+  public static void randomCommand(String[] commandArray, int numCommand) {
+    Random rand = new Random();
+    System.out.printf("Number\tCommand\n");
+    System.out.printf("------\t---------------\n");
+    for (int i = 0; i < numCommand; i++) {
+      int randIndex = rand.nextInt(commandArray.length);
+      System.out.printf("%04d\t%s\n", i, commandArray[randIndex]);
     }
+  }
+
+  // print command array
+  public static void print(String[] commandArray) {
+    System.out.printf("Number\tCommand\n");
+    System.out.printf("------\t---------------\n");
+    for (int i = 0; i < commandArray.length; i++) {
+      System.out.printf("%04d\t%s\n", i, commandArray[i]);
+    }
+  }
+
+  // get array of commands
+  public static String[] getCommandArray(JSONArray commandArray) {
+    String[] arr = new String[commandArray.size()];
+
+    // get names from json object
+    for (int i = 0; i < commandArray.size(); i++) {
+      String command = commandArray.get(i).toString();
+      arr[i] = command;
+    }
+    return arr;
+  }
 }
